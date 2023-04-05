422041d5fec6e4faec7eedc3658411de07cf0c15
chadyuu <yutaro.nishiyama.07@gmail.com>
2023-04-05 21:51:09 +0900

    run 100 epochs

--- auc/auc.py
+++ auc/auc.py
@@ -10,16 +10,30 @@ print(test_data[1])
 print(len(test_data[1])) # 1507
 '''
 
-#dir = '../gram_hf'
-dir = '../gram_hf_no_attention'
+dir = '../gram_hf'
+#dir = '../gram_hf_no_attention'
 
-for train_ratio in np.arange(0.07,0.77,0.07):
+results = [(0.07,4),
+           (0.14,51),
+           (0.21,84),
+           (0.28,60),
+           (0.35,39),
+           (0.42,28),
+           (0.49,38),
+           (0.56,40),
+           (0.63,39),
+           (0.7,46),
+           ]
+#for train_ratio in np.arange(0.07,0.77,0.07):
+for train_ratio, epoch in results:
     with open(f'{dir}/gram_hf_{round(train_ratio,2)}.test_set', 'rb') as f:
         test_data = pickle.load(f)
-    with open(f'{dir}/gram_hf_{round(train_ratio,2)}_0.test_probs', 'rb') as f:
+    with open(f'{dir}/gram_hf_{round(train_ratio,2)}_{epoch}.test_probs', 'rb') as f:
         y_pred_probs = pickle.load(f, encoding='latin1')
     
     y_true_labels = test_data[1]
+    for i, y in enumerate(y_true_labels):
+        y_true_labels[i] = y > 0.5
     auc = roc_auc_score(y_true_labels, y_pred_probs)
 
     print(f"AUC: {auc:.2f}, train_ratio: {round(train_ratio,2)}")
--- run_10times.py
+++ run_10times.py
@@ -1,6 +1,6 @@
 import numpy as np
-#import gram_hf
-import gram_hf_no_attention as gram_hf
+import gram_hf
+#import gram_hf_no_attention as gram_hf
 
 if __name__ == '__main__':
     for train_ratio in np.arange(0.07,0.77,0.07):
@@ -8,8 +8,8 @@ if __name__ == '__main__':
         seq_file = 'output2/output2.seqs'
         tree_file = 'output2/output2'
         label_file = 'output/output.hfs'
-        #out_file = 'gram_hf/gram_hf'
-        out_file = 'gram_hf_no_attention/gram_hf' # for no attention
+        out_file = 'gram_hf/gram_hf'
+        #out_file = 'gram_hf_no_attention/gram_hf' # for no attention
         inputDimSize = gram_hf.calculate_dimSize(seq_file)
         numClass = 1 # predicte a binary value (heart failure)
         numAncestors = gram_hf.get_rootCode(tree_file+'.level2.pk') - inputDimSize + 1
@@ -27,7 +27,7 @@ if __name__ == '__main__':
             hiddenDimSize=128,
             attentionDimSize=128,
             batchSize=100,
-            max_epochs=1,#args.n_epochs,
+            max_epochs=100,#args.n_epochs,
             L2=0.001,
             dropoutRate=0.5,
             logEps=1e-8,
